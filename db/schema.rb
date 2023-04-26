20ee05c4692086c6fde22e746ff2148ac8edb932
Ganmo3 <131627788+Ganmo3@users.noreply.github.com>
2023-04-26 02:45:13 +0000

    [Fix] model & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_25_203704) do
+ActiveRecord::Schema.define(version: 2023_04_26_023940) do
 
-  create_table "bookers", force: :cascade do |t|
+  create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
     t.datetime "created_at", precision: 6, null: false
